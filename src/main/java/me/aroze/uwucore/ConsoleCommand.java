8e1f807937ae0659c62e9678f8815f729525b9d1
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-04-24 22:13:04 +0100

    Added console command

--- src/main/java/me/aroze/uwucore/ConsoleCommand.java
+++ src/main/java/me/aroze/uwucore/ConsoleCommand.java
@@ -1,5 +1,7 @@
 package me.aroze.uwucore;
 
+import me.aroze.uwucore.Util.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,6 +11,10 @@ public class ConsoleCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        String fullCommand = String.join(" ", args);
+
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), fullCommand);
+        sender.sendMessage(ChatUtils.color("&a✔ &2&l| &aExecuted console command: &7/" + fullCommand));
         return true;
 
     }
