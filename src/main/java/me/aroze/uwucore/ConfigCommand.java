9cb99feaf4f525aac43fbcfc529c9cd99f020e3f
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-04-25 23:32:23 +0100

    Added a method for avoiding config breaking: preventing players from changing the object type via /config set.
    this shit took way too long

--- src/main/java/me/aroze/uwucore/ConfigCommand.java
+++ src/main/java/me/aroze/uwucore/ConfigCommand.java
@@ -1,6 +1,7 @@
 package me.aroze.uwucore;
 
 import me.aroze.uwucore.Util.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,9 +29,9 @@ public class ConfigCommand implements CommandExecutor {
                     player.sendMessage(ChatUtils.color("\n&e&lConfig:\n"));
                     player.sendMessage(ChatUtils.color("  &7testWord: &f" + main.getConfig().getString("testWord")));
                     player.sendMessage(ChatUtils.color("  &7testNumber: &f" + main.getConfig().getInt("testNumber") + ""));
-                    if (main.getConfig().getBoolean("testBoolean")) {
-                        player.sendMessage(ChatUtils.color("  &7testBoolean: &atrue"));
-                    } else {player.sendMessage(ChatUtils.color("  &7testBoolean: &cfalse"));}
+                    if (!main.getConfig().getBoolean("testBoolean")) {
+                        player.sendMessage(ChatUtils.color("  &7testBoolean: &cfalse"));
+                    } else {player.sendMessage(ChatUtils.color("  &7testBoolean: &atrue"));}
 
                     player.sendMessage(ChatUtils.color("\n  &7testStringList:"));
                     for (String string : main.getConfig().getStringList("testStringList")) {
@@ -44,10 +45,34 @@ public class ConfigCommand implements CommandExecutor {
 
             if (args.length == 3) {
 
-                if (args[0].equalsIgnoreCase("setvalue")) {
+                if (args[0].equalsIgnoreCase("set")) {
 
                     if (main.getConfig().contains(args[1])) {
-                        main.getConfig().set(args[1], args[2]);
+
+                        Object newValue;
+
+                        try {
+                            newValue = Integer.parseInt(args[2]);
+                        } catch (NumberFormatException a) {
+                            try {
+                                newValue = Double.parseDouble(args[2]);
+                            } catch (NumberFormatException b) {
+                                if (args[2].equalsIgnoreCase("true") || args[2].equalsIgnoreCase("false")) {
+                                    newValue = Boolean.parseBoolean(args[2]);
+                                } else {
+                                    newValue = args[2];
+                                }
+                            }
+                        }
+
+                        if (!main.getConfig().get(args[1]).getClass().getName().equals(newValue.getClass().getName()) && !(main.getConfig().get(args[1]) instanceof String) ) {
+                            player.sendMessage(ChatUtils.color("&c&l⚠ &7You're attempting to change the object type!"));
+                            return true;
+                        } else newValue = newValue.toString();
+
+
+
+                        main.getConfig().set(args[1], newValue);
                         player.sendMessage(ChatUtils.color("&a✔ &7Set &a" + args[1] + " &7to &a" + args[2]));
                         main.saveConfig();
                     } else {
