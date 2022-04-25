79e025a6d3f86795039d2f2a6b1cb25a2e998cbe
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-04-25 19:27:46 +0100

    Revert "Added console command"
    This reverts commit 376e8fbd9610107d07ee76cd0d55445aec5e586f.

--- src/main/java/me/aroze/uwucore/UwUCore.java
+++ src/main/java/me/aroze/uwucore/UwUCore.java
@@ -7,11 +7,6 @@ public final class UwUCore extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
-        getConfig().options().copyDefaults();
-        saveDefaultConfig();
-
-        getCommand("config").setExecutor(new ConfigCommand(this));
         getCommand("heal").setExecutor(new HealCommand());
         getCommand("broadcast").setExecutor(new BroadcastCommand());
         getCommand("console").setExecutor(new ConsoleCommand());
