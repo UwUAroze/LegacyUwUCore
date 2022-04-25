83067977a6a914f563a1ad0cfcb1b8616cea1af0
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-04-25 20:07:21 +0100

    Hex colour support

--- src/main/java/me/aroze/uwucore/Util/ChatUtils.java
+++ src/main/java/me/aroze/uwucore/Util/ChatUtils.java
@@ -2,10 +2,21 @@ package me.aroze.uwucore.Util;
 
 import net.md_5.bungee.api.ChatColor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ChatUtils {
 
-    public static String color(String string) {
-        return ChatColor.translateAlternateColorCodes('&', string);
+    private static final Pattern hexPattern = Pattern.compile("&#[a-fA-F0-9]{6}");
+
+    public static String color(String text) {
+        Matcher match = hexPattern.matcher(text);
+        while (match.find()) {
+            String color = text.substring(match.start(), match.end());
+            text = text.replace(color, ChatColor.of(color.substring(1)).toString());
+            match = hexPattern.matcher(text);
+        }
+        return ChatColor.translateAlternateColorCodes('&', text);
     }
 
 }
