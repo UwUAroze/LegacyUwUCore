0e19a2fd2b8d7e590ba2ac73a4a1839decb97e19
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-04-26 02:22:54 +0100

    Reformatted /heal messages, again

--- src/main/java/me/aroze/uwucore/HealCommand.java
+++ src/main/java/me/aroze/uwucore/HealCommand.java
@@ -21,9 +21,9 @@ public class HealCommand implements CommandExecutor {
             try {
                 player = Bukkit.getPlayer(args[0]);
                 sender.sendMessage(ChatUtils.color("&#ffd4e3You have healed &#eb9bb7" + player.getName()));
-                player.sendMessage(ChatUtils.color("&aYou have been healed by &#eb9bb7" + sender.getName()));
+                player.sendMessage(ChatUtils.color("&#ffd4e3You have been healed by &#eb9bb7" + sender.getName()));
             } catch (NullPointerException e) {
-                sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6e There is no player named &#ff6e6e" + args[0]));
+                sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eThere is no player named &#ff6e6e" + args[0]));
                 return true;
             }
 
