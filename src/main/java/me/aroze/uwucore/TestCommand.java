01cf6fec6373a13250097e5fa8380cb0eed4a27e
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-04-27 05:28:25 +0100

    Added a dancing armour stand... /shrug

---
+++ src/main/java/me/aroze/uwucore/TestCommand.java
@@ -0,0 +1,36 @@
+package me.aroze.uwucore;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestCommand implements CommandExecutor {
+
+    float yaw;
+    float pitch;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        ArmorStand stand = (ArmorStand) Bukkit.getWorld("flat").spawnEntity(new Location(Bukkit.getWorld("flat"), 0, -60, 0), EntityType.ARMOR_STAND);
+
+        AtomicInteger i = new AtomicInteger(100);
+        Bukkit.getScheduler().runTaskTimer(UwUCore.getInstance(), (task) -> {
+            if (i.get() == 0) {
+                task.cancel();
+                return;
+            }
+            i.getAndDecrement();
+            yaw = (float) Math.random()*180;
+            pitch = (float) Math.random()*90;
+            stand.setRotation(yaw,pitch);;
+        }, 0L, 1);
+
+        return true;
+    }
+}
