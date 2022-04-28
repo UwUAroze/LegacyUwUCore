ddf48f3d83df6bba9dc84b0cd3ab7536276536db
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-04-28 08:03:13 +0100

    Custom kb

--- src/main/java/me/aroze/uwucore/Listeners/Knockback.java
+++ src/main/java/me/aroze/uwucore/Listeners/Knockback.java
@@ -3,7 +3,9 @@ package me.aroze.uwucore.Listeners;
 import me.aroze.uwucore.Util.ChatUtils;
 import me.aroze.uwucore.UwUCore;
 import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,18 +17,32 @@ import org.bukkit.util.Vector;
 public class Knockback implements Listener {
 
     @EventHandler
-    public void uhh(PlayerVelocityEvent e) {
-        Vector vel = e.getVelocity();
+    public void customKB(PlayerVelocityEvent e) {
+        Vector vel = e.getPlayer().getVelocity().clone();
 
         Bukkit.broadcastMessage(ChatUtils.color("&a" + vel.getX() + ", " + vel.getY() + ", " + vel.getZ()));
 
-        vel.setX(vel.getX() * UwUCore.getInstance().getConfig().getDouble("Knockback.X"));
-        vel.setY(vel.getY() * UwUCore.getInstance().getConfig().getDouble("Knockback.Y"));
-        vel.setZ(vel.getZ() * UwUCore.getInstance().getConfig().getDouble("Knockback.Z"));
 
+        vel.setX(vel.getX() * UwUCore.getInstance().getConfig().getDouble("Knockback.XZ"));
+        vel.setY(vel.getY() * UwUCore.getInstance().getConfig().getDouble("Knockback.Y"));
+        vel.setZ(vel.getZ() * UwUCore.getInstance().getConfig().getDouble("Knockback.XZ"));
+
+        e.setVelocity(vel);
 
 
         Bukkit.broadcastMessage(ChatUtils.color("&e" + vel.getX() + ", " + vel.getY() + ", " + vel.getZ() + "\n"));
+
+    }
+
+    @EventHandler
+    public void customHitDelay(EntityDamageByEntityEvent e) {
+
+        if (!(e.getDamager() instanceof Player)) return;
+
+        LivingEntity player = (LivingEntity) e.getEntity();
+        player.setNoDamageTicks(UwUCore.getInstance().getConfig().getInt("noDamageTicks"));
+        player.setMaximumNoDamageTicks(UwUCore.getInstance().getConfig().getInt("noDamageTicks"));
+        player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(UwUCore.getInstance().getConfig().getInt("attackSpeed"));
 
     }
 
