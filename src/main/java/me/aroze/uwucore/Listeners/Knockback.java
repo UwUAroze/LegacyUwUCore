d5332040ca692f513a0eac614c4b8af97ad06453
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-04-28 02:31:45 +0100

    test

--- src/main/java/me/aroze/uwucore/Listeners/Knockback.java
+++ src/main/java/me/aroze/uwucore/Listeners/Knockback.java
@@ -1,15 +1,34 @@
 package me.aroze.uwucore.Listeners;
 
+import me.aroze.uwucore.Util.ChatUtils;
+import me.aroze.uwucore.UwUCore;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.util.Vector;
 
 public class Knockback implements Listener {
 
     @EventHandler
-    public void kbGoBrrrr(EntityDamageEvent e) {
-        e.getEntity().setVelocity(new Vector(1.5,1.5,1.5));
+    public void uhh(PlayerVelocityEvent e) {
+        Vector vel = e.getVelocity();
+
+        Bukkit.broadcastMessage(ChatUtils.color("&a" + vel.getX() + ", " + vel.getY() + ", " + vel.getZ()));
+
+        vel.setX(vel.getX() * UwUCore.getInstance().getConfig().getDouble("Knockback.X"));
+        vel.setY(vel.getY() * UwUCore.getInstance().getConfig().getDouble("Knockback.Y"));
+        vel.setZ(vel.getZ() * UwUCore.getInstance().getConfig().getDouble("Knockback.Z"));
+
+
+
+        Bukkit.broadcastMessage(ChatUtils.color("&e" + vel.getX() + ", " + vel.getY() + ", " + vel.getZ() + "\n"));
+
     }
+
 
 }
