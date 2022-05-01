cfedbd267f5a52367959ffb1e10ec0c5abbe48fd
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-01 09:13:10 +0100

    Added a default tab, will be customizable soon.. maybe? i think??

--- src/main/java/me/aroze/uwucore/Listeners/JoinQuit.java
+++ src/main/java/me/aroze/uwucore/Listeners/JoinQuit.java
@@ -3,6 +3,7 @@ package me.aroze.uwucore.Listeners;
 import me.aroze.uwucore.Util.ChatUtils;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -15,6 +16,18 @@ public class JoinQuit implements Listener {
         e.setJoinMessage(ChatUtils.color("&#ffb5cf[&d+&#ffb5cf] " + e.getPlayer().getName()));
         e.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatUtils.color("&#ffd4e3Welcome back!!")));
         e.getPlayer().sendTitle(ChatUtils.color("&#ffb5cfWelcome back"), ChatUtils.color("&#ffd4e3Enjoy your stay <3"),30,75,15);
+        e.getPlayer().setPlayerListHeaderFooter(
+                ChatUtils.color("&#eb9bb7▶&8&m                                                  &#eb9bb7◀\n" +
+                        "\n" +
+                        "&#ffb5cfWelcome to &#9c89c7Server name!\n" +
+                        "&#ffb5cfThere are &#ffe6ef" + Bukkit.getOnlinePlayers().size() + " &#ffb5cfonline players!\n"),
+
+                ChatUtils.color("\n" +
+                        "&#eb9bb7Discord &8&l| &#ffb5cfdiscord.gg/abcd\n" +
+                        "&#a16a7dWebstore &8&l| &#ffb5cf/buy\n" +
+                        "\n" +
+                        "&#eb9bb7▶&8&m                                                  &#eb9bb7◀")
+        );
     }
 
     @EventHandler
