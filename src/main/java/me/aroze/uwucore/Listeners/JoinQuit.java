28712f5aeafb6e69b5f8858f1dbb4dbb98b973fb
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-01 08:51:43 +0100

    Added join action bar

--- src/main/java/me/aroze/uwucore/Listeners/JoinQuit.java
+++ src/main/java/me/aroze/uwucore/Listeners/JoinQuit.java
@@ -1,6 +1,8 @@
 package me.aroze.uwucore.Listeners;
 
 import me.aroze.uwucore.Util.ChatUtils;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -11,6 +13,7 @@ public class JoinQuit implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         e.setJoinMessage(ChatUtils.color("&#ffb5cf[&d+&#ffb5cf] " + e.getPlayer().getName()));
+        e.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatUtils.color("&#ffd4e3Welcome back!!")));
         e.getPlayer().sendTitle(ChatUtils.color("&#ffb5cfWelcome back"), ChatUtils.color("&#ffd4e3Enjoy your stay <3"),30,75,15);
     }
 
