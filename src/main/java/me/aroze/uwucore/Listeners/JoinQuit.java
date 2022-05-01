48a6b240071096044f17a8f2e5462b8a59339de7
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-01 05:47:36 +0100

    Added join title

--- src/main/java/me/aroze/uwucore/Listeners/JoinQuit.java
+++ src/main/java/me/aroze/uwucore/Listeners/JoinQuit.java
@@ -1,5 +1,6 @@
 package me.aroze.uwucore.Listeners;
 
+import me.aroze.uwucore.Util.ChatUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,6 +11,7 @@ public class JoinQuit implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         e.setJoinMessage("&#ffb5cf[&d+&#ffb5cf] " + e.getPlayer().getName());
+        e.getPlayer().sendTitle(ChatUtils.color("&#ffb5cfWelcome back"), ChatUtils.color("&#ffd4e3Enjoy your stay <3"),30,75,15);
     }
 
     @EventHandler
