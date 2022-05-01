d1f6cc21e6b7e7e85dfc07ba8c9430ff4920de38
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-01 02:27:33 +0100

    Added support for hex codes in custom armor colour

--- src/main/java/me/aroze/uwucore/Commands/rgbArmorCommand.java
+++ src/main/java/me/aroze/uwucore/Commands/rgbArmorCommand.java
@@ -1,7 +1,6 @@
 package me.aroze.uwucore.Commands;
 
 import me.aroze.uwucore.Util.ChatUtils;
-import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,19 +8,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.Color;
 
 public class rgbArmorCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (args.length > 3) {
-            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou only need 3 arguments!"));
-            return true;
-        } if (args.length < 3) {
-            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou need 3 arguments!"));
-            return true;
-        }
 
         ItemStack helmet = new ItemStack(Material.LEATHER_HELMET);
         ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
@@ -31,34 +23,59 @@ public class rgbArmorCommand implements CommandExecutor {
         LeatherArmorMeta chestMeta = (LeatherArmorMeta) chestplate.getItemMeta();
         LeatherArmorMeta legsMeta = (LeatherArmorMeta) leggings.getItemMeta();
         LeatherArmorMeta bootsMeta = (LeatherArmorMeta) boots.getItemMeta();
-        try {
-            int red = Integer.parseInt(args[0]);
-            int green = Integer.parseInt(args[1]);
-            int blue = Integer.parseInt(args[2]);
-            if (red > 255 || red < 0 || green > 255 || green < 0 || blue > 255 || blue < 0) {
-                sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou can only have RGB values between 0 and 255!"));
-                return true;
+
+        Color color = Color.WHITE;
+
+        if (args.length == 1) {
+
+            if (args[0].matches("#[0-9A-Fa-f]{6}")) {
+                int r = Integer.valueOf(args[0].substring(1, 3), 16);
+                int g = Integer.valueOf(args[0].substring(3, 5), 16);
+                int b = Integer.valueOf(args[0].substring(5, 7), 16);
+
+                color = Color.fromRGB(r, g, b);
+            } else {
+                if (args.length > 3) {
+                    sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou only need 3 arguments!"));
+                    return true;
+                } if (args.length < 3) {
+                    sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou need 3 arguments!"));
+                    return true;
+                }
             }
 
-            helmetMeta.setColor(Color.fromRGB(red, green, blue));
-            chestMeta.setColor(Color.fromRGB(red, green, blue));
-            legsMeta.setColor(Color.fromRGB(red, green, blue));
-            bootsMeta.setColor(Color.fromRGB(red, green, blue));
+        }
 
-            helmet.setItemMeta(helmetMeta);
-            chestplate.setItemMeta(chestMeta);
-            leggings.setItemMeta(legsMeta);
-            boots.setItemMeta(bootsMeta);
-
-            ((Player) sender).getInventory().addItem(helmet, chestplate, leggings, boots);
-            String hex = String.format("#%02x%02x%02x", red, green, blue);
-            sender.sendMessage(ChatUtils.color("&#eb9bb7✔ &#ffd4e3Created set of leather armor with rgb values: &" + hex + red + ", " + green + ", " + blue));
-
-
+        try {
+            if (color == Color.WHITE) {
+                int red = Integer.parseInt(args[0]);
+                int green = Integer.parseInt(args[1]);
+                int blue = Integer.parseInt(args[2]);
+                if (red > 255 || red < 0 || green > 255 || green < 0 || blue > 255 || blue < 0) {
+                    sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou can only have RGB values between 0 and 255!"));
+                    return true;
+                }
+            }
         } catch (NumberFormatException e) {
             sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eSmh silly. Use RGB values, so:\n/rgbarmour [0-255] [0-255] [0-255]"));
             return true;
         }
+
+        helmetMeta.setColor(color);
+        chestMeta.setColor(color);
+        legsMeta.setColor(color);
+        bootsMeta.setColor(color);
+
+        helmet.setItemMeta(helmetMeta);
+        chestplate.setItemMeta(chestMeta);
+        leggings.setItemMeta(legsMeta);
+        boots.setItemMeta(bootsMeta);
+
+        ((Player) sender).getInventory().addItem(helmet, chestplate, leggings, boots);
+        String toHex = String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
+        sender.sendMessage(ChatUtils.color(
+            "&#eb9bb7✔ &#ffd4e3Created set of leather armor with rgb values: &" + toHex + color.getRed() + ", " + color.getGreen() + ", " + color.getBlue()
+        ));
 
         return true;
 
