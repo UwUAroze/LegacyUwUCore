c6256aaef702040a4143fd2acd1b39f30352dbac
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-08 00:06:44 +0100

    Added command to get kitty gun

--- src/main/java/me/aroze/uwucore/UwUCore.java
+++ src/main/java/me/aroze/uwucore/UwUCore.java
@@ -20,6 +20,7 @@ public final class UwUCore extends JavaPlugin {
         getCommand("console").setExecutor(new ConsoleCommand());
         getCommand("test").setExecutor(new TestCommand());
         getCommand("colouredarmor").setExecutor(new ColouredArmorCommand());
+        getCommand("kittygun").setExecutor(new KittyGunCommand());
 
         getServer().getPluginManager().registerEvents(new Knockback(), this);
         getServer().getPluginManager().registerEvents(new JoinQuit(), this);
---
+++ src/main/java/me/aroze/uwucore/commands/KittyGunCommand.java
@@ -0,0 +1,25 @@
+package me.aroze.uwucore.commands;
+
+import me.aroze.uwucore.util.ChatUtils;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class KittyGunCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) return true;
+
+        ItemStack gun = new ItemStack(Material.END_ROD);
+        gun.getItemMeta().setDisplayName(ChatUtils.color("&#b9ffb3Kitty Gun"));
+        ((Player) sender).getInventory().addItem(gun);
+
+        return true;
+
+    }
+}
