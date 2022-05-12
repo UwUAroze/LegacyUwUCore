c066c384c4fd467c01d598e546e736123b3cffa4
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-12 22:27:45 +0100

    added a simple temp chat format

--- src/main/java/me/aroze/uwucore/listeners/Chat.java
+++ src/main/java/me/aroze/uwucore/listeners/Chat.java
@@ -1,13 +1,20 @@
 package me.aroze.uwucore.listeners;
 
+import me.aroze.uwucore.util.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.util.ArrayList;
+
 public class Chat implements Listener {
+
+    ArrayList<String> chatLog = new ArrayList<>();
 
     @EventHandler
     public void onChat(org.bukkit.event.player.AsyncPlayerChatEvent e) {
-
+        chatLog.add(e.getMessage());
+        e.setFormat(ChatUtils.color("&c" + e.getPlayer().getName() + "&7: &f" + e.getMessage()));
     }
 
 }
