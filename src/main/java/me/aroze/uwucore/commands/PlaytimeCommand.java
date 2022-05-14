2a40c23d2fda3e0384db4139e74b3c2c9be8d7fa
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-14 22:43:52 +0100

    Fromatted differently

--- src/main/java/me/aroze/uwucore/commands/PlaytimeCommand.java
+++ src/main/java/me/aroze/uwucore/commands/PlaytimeCommand.java
@@ -12,9 +12,10 @@ public class PlaytimeCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (!(sender instanceof Player)) return true;
+        String player = sender.getName();
 
         int ticks = ((Player) sender).getStatistic(Statistic.PLAY_ONE_MINUTE);
-        sender.sendMessage(ChatUtils.color("\n&#947c9cYour Playtime:"));
+        sender.sendMessage(ChatUtils.color("\n&#947c9cPlaytime of &#cda2db" + player + "&#947c9cin different units:"));
         sender.sendMessage(ChatUtils.color("&#93c9c1" + ticks + " &#678580ticks"));
         sender.sendMessage("\n");
         return true;
