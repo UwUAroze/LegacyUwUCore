92b009e32f9288b1d7ade4c6a9f07e42cf9a7550
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-14 23:28:40 +0100

    Added ticksToTimestamp function

---
+++ src/main/java/me/aroze/uwucore/util/MathUtils.java
@@ -0,0 +1,14 @@
+package me.aroze.uwucore.util;
+
+public class MathUtils {
+
+    public static String ticksToTimestamp(long ticks) {
+        double seconds = ticks / 20;
+        double minutes = seconds / 60;
+        double hours = minutes / 60;
+        double days = hours / 24;
+
+        return Math.floor(days) + " days" + Math.floor(hours) + " hours" + Math.floor(minutes) + " minutes" + Math.floor(seconds) + " seconds";
+    }
+
+}
