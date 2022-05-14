df34afa476d2981976f1d0f43ddc4111f96d934b
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-14 19:11:45 +0100

    Made some changes to prevent erroring

--- src/main/java/me/aroze/uwucore/commands/PunishCommand.java
+++ src/main/java/me/aroze/uwucore/commands/PunishCommand.java
@@ -10,10 +10,20 @@ public class PunishCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (!(args.length == 2)) sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eSyntax: /punish <player> <kick/ban/tempban>"));
-        if (!(args[1].equalsIgnoreCase("kick") || args[1].equalsIgnoreCase("ban") || args[1].equalsIgnoreCase("tempban"))) sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eSyntax: /punish <player> <kick/ban/tempban>"));
+        if (!(args.length == 2)) {
+            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eSyntax: /punish <player> <kick/ban/tempban>"));
+            return true;
+        }
 
-        if (Bukkit.getOfflinePlayer(args[0]) == null) sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6ePlayer '" + args[0] + "' not found"));
+        if (!(args[1].equalsIgnoreCase("kick") || args[1].equalsIgnoreCase("ban") || args[1].equalsIgnoreCase("tempban"))) {
+            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eSyntax: /punish <player> <kick/ban/tempban>"));
+            return true;
+        }
+
+        if (Bukkit.getOfflinePlayer(args[0]) == null) {
+            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6ePlayer '" + args[0] + "' not found"));
+            return true;
+        }
 
         if (args[1].equalsIgnoreCase("ban")) {
 
