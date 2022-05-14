1cd1306a0f7c8c4d8a84a2e397a7cfbe851f4497
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-15 00:00:59 +0100

    Fixed ticksToTimestamp calculation

--- src/main/java/me/aroze/uwucore/util/MathUtils.java
+++ src/main/java/me/aroze/uwucore/util/MathUtils.java
@@ -3,10 +3,13 @@ package me.aroze.uwucore.util;
 public class MathUtils {
 
     public static String ticksToTimestamp(int ticks) {
-        double seconds = ticks / 20;
-        double minutes = seconds / 60;
-        double hours = minutes / 60;
-        double days = hours / 24;
+
+        double remainingTicks = ticks;
+
+        int seconds = (ticks/20) % 60;
+        int minutes = (ticks/20/60) % 60;
+        int hours = (ticks/20/60/60) % 24;
+        int days = ticks/20/60/60 / 24;
 
         return Math.floor(days) + " days" + Math.floor(hours) + " hours" + Math.floor(minutes) + " minutes" + Math.floor(seconds) + " seconds";
     }
