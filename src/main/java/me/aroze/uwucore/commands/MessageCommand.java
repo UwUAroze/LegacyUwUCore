0b0efd3cc812a5b3bfc5a7ea9b361902a8d94736
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-15 14:48:18 +0100

    Added messaging system

--- src/main/java/me/aroze/uwucore/commands/MessageCommand.java
+++ src/main/java/me/aroze/uwucore/commands/MessageCommand.java
@@ -1,12 +1,36 @@
 package me.aroze.uwucore.commands;
 
+import me.aroze.uwucore.listeners.Chat;
+import me.aroze.uwucore.util.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class MessageCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+
+        if (!(sender instanceof Player)) return true;
+
+        if (args.length != 2) {
+            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eUsage: /msg <player> <message>"));
+            return true;
+        }
+
+        if (!(Bukkit.getOnlinePlayers().contains(args[0]))) {
+            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eThere is no online player matching '" + args[0] + "'"));
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        String message = ChatUtils.color(args[1]);
+
+        sender.sendMessage("&#9f87d6To: &#b4a2db" + target.getName() + " " + "&#ccbbf0" + message);
+        target.sendMessage("&#9f87d6From: &#b4a2db" + sender.getName() + " " + "&#ccbbf0" + message);
+
+
+        return true;
     }
 }
