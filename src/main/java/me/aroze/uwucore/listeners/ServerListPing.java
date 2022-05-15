63f01602fcdb1427ae7fd313c8d68f0a3796756e
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-15 12:37:07 +0100

    Added server icon, kinda?

--- src/main/java/me/aroze/uwucore/listeners/ServerListPing.java
+++ src/main/java/me/aroze/uwucore/listeners/ServerListPing.java
@@ -6,14 +6,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
+import java.io.File;
 import java.net.URL;
 
 public class ServerListPing implements Listener {
 
     @EventHandler
-    public void onPing(ServerListPingEvent e) {
+    public void onPing(ServerListPingEvent e) throws Exception {
         e.setMaxPlayers(69);
         e.setMotd(ChatUtils.color("&#b7e8cbawozey's supwer coowl dev sewvar\n&#d0b7e8woah!!11!!"));
+        e.setServerIcon(Bukkit.loadServerIcon(new File("plugins/UwUCore/icon.png")));
     }
 
 }
