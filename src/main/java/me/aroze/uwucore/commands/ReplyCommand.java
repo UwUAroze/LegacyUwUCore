edbda502b8178a2fef164f1badc3044e603efe79
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-15 14:37:45 +0100

    Added base for message and reply commands

---
+++ src/main/java/me/aroze/uwucore/commands/ReplyCommand.java
@@ -0,0 +1,12 @@
+package me.aroze.uwucore.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ReplyCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
