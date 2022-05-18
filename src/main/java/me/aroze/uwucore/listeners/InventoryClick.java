1367f79ec8774c4ace25913d8d319b4bfa52517a
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-18 21:47:42 +0100

    Added inv click debug

--- src/main/java/me/aroze/uwucore/listeners/InventoryClick.java
+++ src/main/java/me/aroze/uwucore/listeners/InventoryClick.java
@@ -1,9 +1,18 @@
 package me.aroze.uwucore.listeners;
 
+import me.aroze.uwucore.commands.TestCommand;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class InventoryClick implements Listener {
 
+    @EventHandler
+    public void onInventoryClick(org.bukkit.event.inventory.InventoryClickEvent e) {
+        if (e.getInventory() == TestCommand.inv) {
+            e.setCancelled(true);
+            e.getWhoClicked().sendMessage("§cclicc! §7" + e.getAction());
+        }
+    }
 
 
 }
