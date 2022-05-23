69b034a047e392354a3c09532c2f164d72752943
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-23 21:19:36 +0100

    Added different types of tab completion

--- src/main/java/me/aroze/uwucore/commands/TestTabComplete.java
+++ src/main/java/me/aroze/uwucore/commands/TestTabComplete.java
@@ -3,8 +3,10 @@ package me.aroze.uwucore.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestTabComplete implements TabCompleter {
@@ -15,15 +17,15 @@ public class TestTabComplete implements TabCompleter {
         List<String> tabComplete = new ArrayList<>();
         if (args[0].equals("tabfruit")) {
             if (args.length == 2) {
-                tabComplete.add("Pear");
-                tabComplete.add("Orange");
-                tabComplete.add("Apple");
-                tabComplete.add("Banana");
+                return StringUtil.copyPartialMatches(args[1], Arrays.asList("apple", "banana", "orange", "pear"), new ArrayList<>());
             }
             if (args.length == 3) {
-                tabComplete.add("a");
-                tabComplete.add("b");
-                tabComplete.add("c");
+                tabComplete.add("aaa");
+                tabComplete.add("bbb");
+                tabComplete.add("ccc");
+                tabComplete.add("123");
+                tabComplete.add("dafssdf");
+                tabComplete.add("abc");
             }
         }
         return tabComplete;
