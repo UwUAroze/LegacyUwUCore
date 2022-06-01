b959b38fa1e6960737822ae3f4d5e0cab81c08cb
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-02 00:23:40 +0100

    Error handling

--- src/main/java/me/aroze/uwucore/commands/HatCommand.java
+++ src/main/java/me/aroze/uwucore/commands/HatCommand.java
@@ -3,12 +3,14 @@ package me.aroze.uwucore.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class HatCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-
+        if (!(sender instanceof Player)) return true;
+        Player player = (Player) sender;
 
         return true;
     }
