0ee4f3a8b2c34276aa1ffe8245e6fc09aabf1d21
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-02 00:26:58 +0100

    Finished the bulk of /hat

--- src/main/java/me/aroze/uwucore/commands/HatCommand.java
+++ src/main/java/me/aroze/uwucore/commands/HatCommand.java
@@ -1,10 +1,12 @@
 package me.aroze.uwucore.commands;
 
 import me.aroze.uwucore.util.ChatUtils;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class HatCommand implements CommandExecutor {
     @Override
@@ -14,9 +16,16 @@ public class HatCommand implements CommandExecutor {
         Player player = (Player) sender;
 
         if (!player.hasPermission("uwucore.hat")) {
-            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou aren't allowed to do this! smh!"));
+            player.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou aren't allowed to do this! smh!"));
             return true;
         }
+
+        ItemStack playerTool = new ItemStack(player.getInventory().getItemInMainHand());
+
+        player.getInventory().getItemInMainHand().setType(Material.AIR);
+        player.getInventory().setHelmet(playerTool);
+
+        player.sendMessage(ChatUtils.color("&#eb9bb7✔ &#ffd4e3Woo, your head is now way cooler :flushed: :eyes:"));
 
         return true;
     }
