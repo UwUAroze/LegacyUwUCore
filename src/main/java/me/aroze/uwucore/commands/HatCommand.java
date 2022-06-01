dfcb18f947b8e5bca526dd1cce1822cd2a3a1550
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-02 00:23:51 +0100

    added permission node to /hat

--- src/main/java/me/aroze/uwucore/commands/HatCommand.java
+++ src/main/java/me/aroze/uwucore/commands/HatCommand.java
@@ -1,5 +1,6 @@
 package me.aroze.uwucore.commands;
 
+import me.aroze.uwucore.util.ChatUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,6 +12,11 @@ public class HatCommand implements CommandExecutor {
 
         if (!(sender instanceof Player)) return true;
         Player player = (Player) sender;
+
+        if (!player.hasPermission("uwucore.hat")) {
+            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou aren't allowed to do this! smh!"));
+            return true;
+        }
 
         return true;
     }
