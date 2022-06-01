46ce4691f5563c023b1f79f2eed1394f34960704
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-01 19:16:00 +0100

    Implemented methods oop

--- src/main/java/me/aroze/uwucore/commands/EditCakeCommand.java
+++ src/main/java/me/aroze/uwucore/commands/EditCakeCommand.java
@@ -1,4 +1,12 @@
 package me.aroze.uwucore.commands;
 
-public class EditCakeCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class EditCakeCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
 }
