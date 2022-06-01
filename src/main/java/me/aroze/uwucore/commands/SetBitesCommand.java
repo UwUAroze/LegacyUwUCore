d0ae2b32650b3bd12851d00f0f42c5e2a2f460b4
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-01 23:33:41 +0100

    refactoring

--- src/main/java/me/aroze/uwucore/commands/SetBitesCommand.java
+++ src/main/java/me/aroze/uwucore/commands/SetBitesCommand.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class EditCakeCommand implements CommandExecutor {
+public class SetBitesCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
