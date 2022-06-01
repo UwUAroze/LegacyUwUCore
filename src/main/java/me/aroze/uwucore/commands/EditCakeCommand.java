abb02efa519cc1c5ffa349b45eedc232a310e00f
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-01 19:18:03 +0100

    forgot to register in main class too. heh.

--- src/main/java/me/aroze/uwucore/commands/EditCakeCommand.java
+++ src/main/java/me/aroze/uwucore/commands/EditCakeCommand.java
@@ -7,6 +7,9 @@ import org.bukkit.command.CommandSender;
 public class EditCakeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+
+        //
+
+        return true;
     }
 }
