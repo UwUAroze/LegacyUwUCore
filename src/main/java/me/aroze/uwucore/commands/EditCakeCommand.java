29f7c95f905296248bcd5ce8193283a5ec74c138
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-01 19:31:56 +0100

    Error handling/prevention

--- src/main/java/me/aroze/uwucore/commands/EditCakeCommand.java
+++ src/main/java/me/aroze/uwucore/commands/EditCakeCommand.java
@@ -1,14 +1,36 @@
 package me.aroze.uwucore.commands;
 
+import me.aroze.uwucore.util.ChatUtils;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class EditCakeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        //
+        if (!(sender instanceof Player)) return true;
+        Player player = (Player) sender;
+
+        if (!player.hasPermission("uwucore.editcake")) {
+            sender.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou aren't allowed to do this! smh!"));
+            return true;
+        }
+
+        Block cakeBlock = player.getTargetBlockExact(6);
+
+        if (cakeBlock.getType().isAir()) {
+            player.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou're staring at air smh."));
+            return true;
+        }
+
+        if (!(cakeBlock.getType().equals(Material.CAKE))) {
+            player.sendMessage(ChatUtils.color("&#ff6e6e⚠ &#ff7f6eYou need to be looking at a cake, dummy!"));
+            return true;
+        }
 
         return true;
     }
