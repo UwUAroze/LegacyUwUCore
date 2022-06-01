f4a08b349aea4baf7e5996c28c7bab925d9d6313
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-01 19:32:00 +0100

    aa

--- src/main/java/me/aroze/uwucore/UwUCore.java
+++ src/main/java/me/aroze/uwucore/UwUCore.java
@@ -37,6 +37,7 @@ public final class UwUCore extends JavaPlugin {
         getCommand("playtime").setExecutor(new PlaytimeCommand());
         getCommand("msg").setExecutor(new MessageCommand());
         getCommand("reply").setExecutor(new ReplyCommand());
+        getCommand("editcake").setExecutor(new EditCakeCommand());
 
         getCommand("test").setTabCompleter(new TestTabComplete());
 
