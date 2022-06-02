0e6d599b54a40a802c6b6dc8b2a57d7d3728aa10
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-02 19:48:05 +0100

    Added the method for instant spawn, along with some needed vars

--- src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
+++ src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
@@ -1,14 +1,26 @@
 package me.aroze.uwucore.commands;
 
+import me.aroze.uwucore.util.ChatUtils;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class SpawnCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        if (!(sender instanceof Player)) return true;
+        Player player = (Player) sender;
+
+        Location spawnLoc = player.getWorld().getSpawnLocation(); // for now just gets world spawn, /setspawn soon:tm:
         int spawnTimer = 3; //in seconds and should be a config option soon.
+
+        if (spawnTimer <= 0) {
+            player.teleport(spawnLoc);
+            player.sendMessage(ChatUtils.color("&#eb9bb7✔ &#ffd4e3You've been teleported to spawn. woah!"));
+        }
 
         return true;
     }
