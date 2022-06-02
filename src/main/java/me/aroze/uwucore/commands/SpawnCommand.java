73fedfbefe3a0e3d1cb9ac5e552ccfc740de36a7
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-02 19:41:05 +0100

    oop

--- src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
+++ src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
@@ -1,4 +1,15 @@
 package me.aroze.uwucore.commands;
 
-public class SpawnCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SpawnCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+
+
+        return true;
+    }
 }
