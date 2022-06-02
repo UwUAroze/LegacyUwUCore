b0ffc5a628d585ac1c4341ad43fb4cc1b18d0759
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-02 20:55:50 +0100

    Hopefully fixed spawn timer

--- src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
+++ src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
@@ -2,15 +2,13 @@ package me.aroze.uwucore.commands;
 
 import me.aroze.uwucore.UwUCore;
 import me.aroze.uwucore.util.ChatUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class SpawnCommand implements CommandExecutor {
     @Override
@@ -31,26 +29,30 @@ public class SpawnCommand implements CommandExecutor {
 
         player.sendMessage(ChatUtils.color("&#ffd4e3Try not to move for the next &#eb9bb7" + spawnTimer + " &#ffd4e3or so seconds!"));
 
-        Location startLoc = player.getLocation();
-        AtomicInteger ticks = new AtomicInteger();
 
-        BukkitTask spawnTask = null;
-        BukkitTask finalSpawnTask = spawnTask;
 
-        spawnTask = Bukkit.getScheduler().runTaskTimer(UwUCore.getInstance(), () -> {
-            ticks.getAndIncrement();
+        BukkitTask spawnTask = new BukkitRunnable() {
 
-            if (ticks.get() >= spawnTimer*20)  {
-                player.teleport(spawnLoc);
-                player.sendMessage(ChatUtils.color("&#eb9bb7✔ &#ffd4e3You've been teleported to spawn. woah!"));
-                finalSpawnTask.cancel();
-                return;
+            Location startLoc = player.getLocation();
+            int ticks = 1;
+
+            @Override
+            public void run() {
+                ticks++;
+                if (ticks >= spawnTimer*20)  {
+                    player.teleport(spawnLoc);
+                    player.sendMessage(ChatUtils.color("&#eb9bb7✔ &#ffd4e3You've been teleported to spawn. woah!"));
+                    this.cancel();
+                    return;
+                }
+                if (player.getLocation().distance(startLoc) > 0.5) {
+                    player.sendMessage(ChatUtils.color("a"));
+                    this.cancel();
+                    return;
+                }
             }
 
-            if (player.getLocation().distance(startLoc) > 0.5) {
-                // player moved too much, need to bully
-            }
-        } , 0, 1);
+        }.runTaskTimer(UwUCore.getInstance(), 0, 1);
 
         return true;
     }
