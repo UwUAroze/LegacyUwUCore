c039eb7b256e8802474b626c7440109dd7d361ad
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-02 21:02:25 +0100

    Changed messages

--- src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
+++ src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
@@ -27,7 +27,7 @@ public class SpawnCommand implements CommandExecutor {
             return true;
         }
 
-        player.sendMessage(ChatUtils.color("&#eb9bb7Try not to move &#ffd4e3for the next " + spawnTimer + " or so seconds!"));
+        player.sendMessage(ChatUtils.color("&#eb9bb7Try not to move for the next " + spawnTimer + " or so seconds!"));
 
 
 
