da23d3b11573624eff5c87269d72bc21ab4bbdb7
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-06-02 19:41:54 +0100

    oop

--- src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
+++ src/main/java/me/aroze/uwucore/commands/SpawnCommand.java
@@ -8,7 +8,7 @@ public class SpawnCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-
+        int spawnTimer = 3; //in seconds and should be a config option soon.
 
         return true;
     }
